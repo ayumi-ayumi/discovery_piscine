a4ee10204f08d07ada8bdc415164bd07cec56ddc
Ayumi Sato <ayusato@c4b2c1.42berlin.de>
2025-12-03 14:26:36 +0100

    ADD mouseover & mouseout

--- cell03/ex01/balloon.js
+++ cell03/ex01/balloon.js
@@ -4,16 +4,17 @@ const balloon = document.querySelector(".balloon");
 //     console.error("No element with class 'balloon' found.");
 //     return;
 // }
+
 let colors = ["green", "blue", "red"];
 let i = 0;
 let balloon_height = parseInt(getComputedStyle(balloon).height, 10) || 0;
 let balloon_width = parseInt(getComputedStyle(balloon).width, 10) || 0;
 
+
 balloon.addEventListener('click', () => {
-    if(i == colors.length)
+    if (i == colors.length)
         i = 0;
-    if (balloon_height >= 420 || balloon_width >= 420)
-    {
+    if (balloon_height >= 420 || balloon_width >= 420) {
         balloon_height = 200;
         balloon_width = 200;
     }
@@ -23,4 +24,30 @@ balloon.addEventListener('click', () => {
     balloon.style.height = balloon_height + "px";
     balloon.style.width = balloon_width + "px";
     i++;
+})
+
+balloon.addEventListener('mouseover', () => {
+    if (balloon_height > 200 || balloon_width > 200) {
+        balloon_height -= 5;
+        balloon_width -= 5;
+        console.log(colors[i + 1]);
+        balloon.style.height = balloon_height + "px";
+        balloon.style.width = balloon_width + "px";
+        balloon.style.backgroundColor = colors[i - 1];
+    }
+    if (i == colors.length)
+        i = 0;
+})
+
+balloon.addEventListener('mouseout', () => {
+    if (i == colors.length)
+        i = 0;
+    if (balloon_height > 200 || balloon_width > 200) {
+        balloon_height += 5;
+        balloon_width += 5;
+        balloon.style.height = balloon_height + "px";
+        balloon.style.width = balloon_width + "px";
+        balloon.style.backgroundColor = colors[i + 1];
+    }
+    console.log(colors[i + 1]);
 })
